2ecd753fe9eeac97cce0dca823c9b8b1d4981e22
evandergraaf <evandergraaf@drury.edu>
2020-10-24 12:24:46 -0500

    fixed login redirect

--- public/js/loginUser.js
+++ public/js/loginUser.js
@@ -9,7 +9,7 @@ $(function() {
             }, function (data, status) {
                 console.log(data);
                 console.log(data.token);
-
+                window.location.href = "/homeUsers.html";
             });
 
     });
