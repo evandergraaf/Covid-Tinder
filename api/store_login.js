638a387515606167ffc06706acb155ab4098e0a9
Seth Workman <saworkman1@gmail.com>
2020-10-24 11:52:17 -0500

    carter stuff
    carter stuff

---
+++ api/store_login.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const SQL = require("../db.js");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jwt-simple");
+
+router.use(bodyParser.urlencoded({extended:true}));
+router.use(bodyParser.json());
+
+secret = "scoobydoobydoowhereareyou?";
+
+router.post("/store/login", function(req, res) {
+
+    let qry = "SELECT store_email, password FROM Store WHERE store_email = ?";
+    SQL.query(qry, [req.body.store_email], (err, rows) => {
+
+        if (err) throw err;
+
+        if (rows.length == 0){
+            res.status(401).json({error: "The email or password was incorrect."});
+        }
+        else{
+            bcrypt.compare(req.body.password, rows[0].password, (err, valid)=>{
+                if (err){
+                    res.status(400).json({error: err});
+                }
+                else if (valid){
+                    //Optional personal information to return
+                    //let commaPos = rows[0].full_name.indexOf(",");
+                    //let firstName = rows[0].full_name.substring(commaPos+1);
+                    let token = jwt.encode({username:rows[0].uid}, secret);
+                    res.json({token: token});
+                }
+            });
+        }
+    });
+});
+
+
+module.exports = router;
