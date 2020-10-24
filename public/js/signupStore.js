54a714b2eeb7f8e7e960b4c234b5908fca1298e7
evandergraaf <evandergraaf@drury.edu>
2020-10-24 13:18:45 -0500

    no message

---
+++ public/js/signupStore.js
@@ -0,0 +1,21 @@
+
+$(function() {
+
+    $("#signUpFormStore").submit((e) => {
+        e.preventDefault();
+        if ($("#password").val() === $("#valPass").val()) {
+            $.post("api/store/create", {
+                store_email: $("#store_email").val().toLowerCase(),
+                password: $("#valPass").val().toLowerCase(),
+                company: $("#company").val(),
+                location: "0",
+                phone: $("#tlf").val(),
+            }, function (data, status) {
+                console.log(data);
+            });
+        } else{
+            alert("Passwords don't match");
+        }
+        window.location.href = "/loginCompany.html";
+    });
+})
