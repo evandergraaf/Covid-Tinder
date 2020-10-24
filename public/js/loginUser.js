e8286392b5437bf836e2ce0970b0156b2bad2499
evandergraaf <evandergraaf@drury.edu>
2020-10-24 12:59:29 -0500

    ean animation

--- public/js/loginUser.js
+++ public/js/loginUser.js
@@ -7,18 +7,14 @@ $(function() {
                 user_email: $("#email").val().toLowerCase(),
                 password: $("#password").val().toLowerCase(),
             }, function (data, status) {
-<<<<<<< HEAD
                 console.log(data);
                 console.log(data.token);
                 $(".card").slideUp();
                 setTimeout(function(){
                     window.location.href = "/homeUsers.html";
                 }, 1000);
-                
-=======
                 window.localStorage.setItem("token", data.token);
                 window.location.href = "/homeUsers.html";
->>>>>>> a68494cd021ad7014045110fdeb827c27934668d
             });
 
     });
