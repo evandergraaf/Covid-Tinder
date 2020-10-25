76640e656c6bbaa89adf270882e2122aaa91e86a
Laura Pareja-Prieto <lpareja@drury.edu>
2020-10-25 14:37:25 -0500

    clean code

--- public/js/jobRetrieveHomeCompany.js
+++ public/js/jobRetrieveHomeCompany.js
@@ -13,17 +13,19 @@ $(function(){
                 var idList = [];
                 for (var j=0; j<data.length; j++){
 
-                    // every 4 entries we have to create a new row or close the row
+                    // every 3 entries we have to create a new row or close the row
                     if((counter%3 == 0)){
                         html += "<div class='row'>\n";
                     }
-
+                    
+                    //button id for delete button in the cards
                     var btnID = "delete" + data[j].job_id;
                     idList.push(btnID);
                     
                     // create the card for the job
                     html += "<div class='col-sm'>\n <div class='card' style='width: 20rem;'>\n <div class='card-body'>\n <h5 class='card-title'>" + data[j].job_name + "</h5>\n <p class='card-text'>" + data[j].description + "</p>\n </div>\n <ul class='list-group list-group-flush'>\n <li class='list-group-item'>Duration: " + data[j].start_date + "-" + data[j].end_date + "</li>\n <li class='list-group-item'>Schedule: " + data[j].scheduled_hours + "</li>\n <li class='list-group-item'>Location: " + data[j].address + "</li>\n <li class='list-group-item'>Salary: " + data[j].pay + "$/h</li>\n <li class='list-group-item'>Certifications needed: " + data[j].certifications_needed + "</li>\n <li class='list-group-item'>\n <button type='button' class='btn btn-outline-warning' id=" + btnID + " " + ">Delete Job</button>\n  </li>\n </ul>\n </div>\n </div>\n ";
 
+                    //end a row every 3 cards
                     if((counter != 0) && (counter%3 == 2) || (counter >= data.length+1)){
                         html += "</div>";
                     }
@@ -40,6 +42,7 @@ $(function(){
         });
     };
 
+    //function to delete a job once is added
     function deleteBtn(idList){
         for (let i = 0; i < idList.length; i++){
             $("#" + idList[i]).click(function(){
