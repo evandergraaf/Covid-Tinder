003b99fe6e9c3d4b523da5c1cbb2aec864f54521
evandergraaf <evandergraaf@drury.edu>
2020-10-25 12:15:10 -0500

    Fixed keywords search

--- public/js/jobRetrieveHomeUsers.js
+++ public/js/jobRetrieveHomeUsers.js
@@ -54,7 +54,61 @@ $(function(){
             console.log("html to add: " + html);
             $("#jobCards").html(html);
             applyButton(idList);
-
+            
+     $("#searchBtn").click(() => {
+        var jobs = []
+            var keyword = $("#keywords").val().toLowerCase();
+            $.get("api/job/list",(data)=>{
+                //search based off name
+                for(var i = 0; i<data.length; i++){
+                    var job_name = data[i].job_name.toLowerCase();
+                    if(job_name.includes(keyword)){
+                           jobs.push(data[i]);
+                           data[i]="";
+                    }
+                }
+//                console.log(data);
+                for(var i = 0; i<data.length; i++){
+                    if(data[i] != ""){
+                        var desc = data[i].description.toLowerCase();
+                        var email = data[i].store_email.toLowerCase();
+                        var cert = data[i].certifications_needed.toLowerCase();
+                        
+                        var rest = desc + email + cert;
+                        if(rest.includes(keyword)){
+                            jobs.push(data[i]);
+                        }
+                    }
+                }
+        console.log(jobs.length);
+        console.log(jobs);
+     
+        console.log("jobs: " , jobs.length);
+          console.log(jobs);
+            
+            var counter = 0;
+            var html = "";
+            for (var j=0; j<jobs.length; j++){
+                        if((counter%3 == 0)){
+//                console.log("inside first if");
+                html += "<div class='row'>\n";
+            }
+            // create the card for the job
+            html += "<div class='col-sm'>\n <div class='card' style='width: 20rem;'>\n <div class='card-body'>\n <h5 class='card-title'>" + jobs[j].job_name + "</h5>\n <p class='card-text'>" + jobs[j].description + "</p>\n </div>\n <ul class='list-group list-group-flush'>\n <li class='list-group-item'><b>Duration: </b>" + jobs[j].start_date + "-" + jobs[j].end_date + "</li>\n <li class='list-group-item'><b>Schedule: </b>" + jobs[j].scheduled_hours + "</li>\n <li class='list-group-item'><b>Location: </b>" + jobs[j].location + "</li>\n <li class='list-group-item'><b>Salary: </b>" + jobs[j].pay + "$/h</li>\n <li class='list-group-item'><b>Certifications needed: </b>" + jobs[j].certifications_needed + "</li>\n  <li class='list-group-item'>\n <button type='button' class='btn btn-outline-warning' id=" + btnID + " " + ">Apply</button>\n  </li>\n  </ul>\n </div>\n </div>\n ";
+            
+//            console.log(counter);
+            if((counter != 0) && (counter%3 == 2) || (counter >= data.length+1)){
+//                console.log("inside second if");
+                html += "</div>";
+            }
+            counter = counter + 1;
+        }
+        
+        console.log("html to add: " + html);
+        $("#jobCards").html(html);
+        applyButton(idList);
+          });
+    });
         })
     }
 
