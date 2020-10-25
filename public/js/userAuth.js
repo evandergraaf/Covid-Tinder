1f79089563f94b215cbe4e9042a3a4e7237b08d8
Seth Workman <saworkman1@gmail.com>
2020-10-25 13:27:20 -0500

    slider
    slider

--- public/js/userAuth.js
+++ public/js/userAuth.js
@@ -18,4 +18,13 @@ $(function() {
             console.log(data[0]); }
     });
 
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+
 })
