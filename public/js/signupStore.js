70341ef907b2b53babcf2646a2c29a46ae3fe78a
Carter Williams <cwilliams039@drury.edu>
2020-10-24 20:11:42 -0500

    Stores now have their coordinates stored!

--- public/js/signupStore.js
+++ public/js/signupStore.js
@@ -3,23 +3,29 @@ $(function() {
 
     $("#signUpFormStore").submit((e) => {
         e.preventDefault();
-        if ($("#password").val() === $("#valPass").val()) {
-            $.post("api/store/create", {
-                store_email: $("#store_email").val().toLowerCase(),
-                password: $("#valPass").val().toLowerCase(),
-                company: $("#company").val(),
-                location: "0",
-                phone: $("#tlf").val(),
-            }, function (data, status) {
-                console.log(data);
-                $(".card").slideUp();
-                setTimeout(function(){
-                    window.location.href = "/logInCompany.html";
-                }, 1000);
-            });
-        } else{
-            alert("Passwords don't match");
-        }
+        $.get("https://maps.googleapis.com/maps/api/geocode/json?address="+ $("#location").val() +"&key=AIzaSyAWd49rKgMR0RHaL9a38_tyQEEhXudKgrc",
+                function(res, err){
+                    var latitude = res.results[0]["geometry"].location.lat;
+                    var longitude = res.results[0]["geometry"].location.lng;
+                    var location =  latitude + ',' + longitude;
+                    if ($("#password").val() === $("#valPass").val()) {
+                        $.post("api/store/create", {
+                            store_email: $("#store_email").val().toLowerCase(),
+                            password: $("#valPass").val().toLowerCase(),
+                            company: $("#company").val(),
+                            location: location,
+                            phone: $("#tlf").val(),
+                        }, function (data, status) {
+                            console.log(data);
+                            $(".card").slideUp();
+                            setTimeout(function(){
+                            window.location.href = "/logInCompany.html";
+                            }, 1000);
+                        });
+                    } else{
+                        alert("Passwords don't match");
+                    }
+                });
 
     });
 })
