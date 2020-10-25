7a3f0ebf997d8c2136f2a99d8e09301a858800c0
Laura Pareja-Prieto <lpareja@drury.edu>
2020-10-25 14:24:37 -0500

    clean code

--- public/js/loginUser.js
+++ public/js/loginUser.js
@@ -1,18 +1,12 @@
 
 $(function() {
-//   console.log("loaded");
-//    $(".card").style.display = "none";
-//    setTimeout(function(){
-//                    $(".card").slideDown();
-//                }, 1000);
+
     $("#loginUserForm").submit((e) => {
         e.preventDefault();
             $.post("api/user/login", {
                 user_email: $("#email").val().toLowerCase(),
                 password: $("#password").val().toLowerCase(),
             }, function (data, status) {
-                console.log(data);
-                console.log(data.token);
                 $(".card").slideUp();
                 setTimeout(function(){
                     window.location.href = "/homeUsers.html";
